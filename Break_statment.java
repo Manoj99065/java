c83d12966baacc2fb57711b56dc002690c557e63
Manoj Thakur <manoj99065@gmail.com>
2025-03-08 00:30:52 +0530

    Create Break_statment.java
    Break Statement in java ? 
    In java break statement is uses to transfer the control out of loop when desire condition is meet then such is called break statement in java

---
+++ Break_statment.java
@@ -0,0 +1,12 @@
+class break_statment {
+   public static void main(String args[]) {
+      int i = 0;
+      while (i < 20) {
+         if (i == 19) {
+            break;
+         }
+         i++;
+      System.out.println("Hi This is my output = " + i);
+      }
+   }
+}
